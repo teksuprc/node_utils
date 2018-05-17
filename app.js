6e30c5539fd21f92bc32a6744aacdf4294686240
teksuprc <teksuprc@yahoo.com>
2018-05-17 15:39:49 -0400

    added more fields to images.json.

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const DIR = "C:\\Users\\chandlerru\\Documents\\DEV\\projects\\ng6-image-gallery\
 const THUMBDIR = "C:\\Users\\chandlerru\\Documents\\DEV\\projects\\ng6-image-gallery\\src\\data\\thumbs";
 
 
-var Image = function(id,e, n,u,w,h,a,c,t,tw,th) {
+var Image = function(id,e,n,u,w,h,a,c,t,tw,th,l,cat) {
     return {
         "id": id,
         "ext": e,
@@ -20,9 +20,13 @@ var Image = function(id,e, n,u,w,h,a,c,t,tw,th) {
         "created": c,
         "thumbnail": t,
         "thumbWidth": tw,
-        "thumbHeight": th
+        "thumbHeight": th,
+        "label": l,
+        "category": cat
     };
 }
+
+var categories = ['one', 'two', 'three', 'four', 'null', 'null', 'null'];
 
 var cnt = 0;
 
@@ -39,6 +43,11 @@ var getAspectRatio = (w,h) => {
     return 0;
 }
 
+var getRandomCategory = () => {
+    let r = Math.floor(Math.random() * 7);
+    return categories[r];
+}
+
 var createThumbnail = (name, inFile, outFile) => {
     sharp(inFile).resize(200).toFile(outFile, (err, info) => {
         if(err)
@@ -53,17 +62,20 @@ var readDir = (dirPath) => {
         var inFile = path.join(dirPath, file);
         var outFile = path.join(THUMBDIR, file);
         var stats = fs.statSync(inFile);
+        var cat = getRandomCategory();
+        cat = (cat === 'null') ? null : cat;
+        console.log(cat);
 
         var ext = path.extname(inFile);
         if(ext === '.mp4') {
-            var img = new Image(cnt++, ext.substring(1), file, "../data/images/" + file, 0, 0, 0, stats.ctime.toISOString(), "../data/thumbs/" + file, 200, 200);
+            var img = new Image(cnt++, ext.substring(1), file, "../data/images/" + file, 0, 0, 0, stats.ctime.toISOString(), "../data/thumbs/" + file, 200, 200, file, cat);
             return list.concat(isDir ? readDir(inFile) : [img]);
         }
 
         var size = sizeOf(inFile);
         var aspect = getAspectRatio(size.width, size.height);
         var tsize = createThumbnail(file, inFile, outFile);
-        var img = new Image(cnt++, ext.substring(1), file, "../data/images/" + file, size.width, size.height, aspect, stats.ctime.toISOString(), "../data/thumbs/" + file, tsize.tw, tsize.th);
+        var img = new Image(cnt++, ext.substring(1), file, "../data/images/" + file, size.width, size.height, aspect, stats.ctime.toISOString(), "../data/thumbs/" + file, tsize.tw, tsize.th, file, cat);
         var isDir = fs.statSync(inFile).isDirectory();
         return list.concat(isDir ? readDir(inFile) : [img]);
     }, []);
